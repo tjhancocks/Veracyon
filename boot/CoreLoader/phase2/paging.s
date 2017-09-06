c42061f7ac99556b6844f0d1a985aef8a3f9d1a3
Tom Hancocks <tomhancocks@gmail.com>
2017-09-06 17:13:35 +0100

    Fully implement basic paging solution for VESA display modes.
    This setups the page map for VESA display memory, and applies a
    default background color to the display.

--- boot/CoreLoader/phase2/paging.s
+++ boot/CoreLoader/phase2/paging.s
@@ -21,6 +21,13 @@
 	[bits	32]
 
 ;;
+;; The following defines the layout of the paging state structure.
+;;
+STRUC PagingState
+	.next_frame				resd 1
+ENDSTRUC
+
+;;
 ;; Setup the basic paging environment and identity map the lowest 1MiB of
 ;; memory. This will ensure that the BootLoader and any early code/data is still
 ;; addressable once paging is enabled. In this the Page Table must be 
@@ -38,6 +45,9 @@ _prepare_paging:
 		xor eax, eax
 		cld
 		rep stosd
+	.frames:
+		mov edi, 0xF800					; Location of the Paging State structure
+		mov dword[edi + PagingState.next_frame], 0x3F ; 1 MiB, Frame 63
 	.epilogue:
 		mov esp, ebp
 		pop ebp
@@ -125,14 +135,50 @@ _prepare_lfb_page_tables:
 	.prologue:
 		push ebp
 		mov ebp, esp
+		push 0							; [ebp - 4] Page table count
+		push 0							; [ebp - 8] Linear Frame Buffer Base
+		push 0							; [ebp - 12] Linear Frame Buffer Size
 	.required_test:
 		mov esi, 0xFE00
 		cmp byte[esi], 0
 		jz .unrequired
-		xchg bx, bx
-		cli
-		hlt
-		jmp $
+	.calculate_page_table_count:
+		xor edx, edx
+		mov eax, [esi + 0x29]			; Fetch the linear frame buffer
+		mov [ebp - 8], eax				; Store it.
+		mov eax, [esi + 0x39]			; Fetch the screen size in bytes
+		mov [ebp - 12], eax
+		shr eax, 24						; Calcuate how many page tables are
+		mov [ebp - 4], eax				; required and save it
+		mov ecx, eax
+		add ecx, 1
+		mov edi, 0x12000				; Location of root page directory
+		mov eax, 0x17000				; First VESA page table is at...
+		mov ebx, [ebp - 8]				; Restore the linear frame buffer base
+		shr ebx, 22						; Get the page directory offset
+	.next_page_table:
+		or eax, 0x03					; Apply attributes to the frame address
+		mov dword[edi + ebx * 4], eax	; Store the page table frame
+		add eax, 0x1000					; Move to the start of the next frame
+		loop .next_page_table
+	.setup_lfb_frames:
+		mov eax, [ebp - 8]				; Fetch the linear frame buffer base
+		or eax, 0x03					; Set appropriate attributes on entry 
+		mov ecx, [ebp - 12]				; Fetch the linear frame buffer size
+		shr ecx, 12						; Divide by the size of a page
+		add ecx, 1
+		mov edi, 0x17000				; Location of the first VESA Page Table
+	.allocate_lfb_frame:
+		stosd
+		add eax, 0x1000
+		loop .allocate_lfb_frame
+	.clear_screen:
+		mov edi, [ebp - 8]				; Fetch the linear frame buffer location
+		mov ecx, [ebp - 12]				; Fetch the linear frame buffer size
+		shr ecx, 2						; Divide by 4
+		mov esi, 0xFE00
+		mov eax, [esi + 0x2d]			; Fetch the default background color
+		rep stosd
 	.unrequired:
 		push strings.unrequired
 		call _send_serial_bytes
