d4557da0e81fe10bdde0ae2fe0fb0962713c1613
Tom Hancocks <tomhancocks@gmail.com>
2017-10-05 11:14:16 +0100

    Fix build issues present in previous commit.
    The issues were related to Makefile build intermediates, and syntax
    errors.

--- kernel/include/va_args.h
+++ kernel/include/va_args.h
@@ -24,6 +24,7 @@
 #define __VKERNEL_VA_ARGS__
 
 typedef __builtin_va_list va_list;
+
 #define va_start(ap,last) __builtin_va_start(ap, last)
 #define va_end(ap) __builtin_va_end(ap)
 #define va_arg(ap,type) __builtin_va_arg(ap,type)
